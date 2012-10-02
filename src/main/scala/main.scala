8f84943941cd9bed1901fda1970d7dc770683922
softprops <d.tangren@gmail.com>
2012-10-01 23:15:37 -0400

    debugging transforms

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -2,15 +2,26 @@ package hostclub
 
 object Main {
   def main(args: Array[String]) {
-    def printres = println(io.Source.fromFile(new java.io.File("test.out")).getLines().mkString("\n"))
-    println("\ndefault")
-    Hosts(identity)
-    printres
-    println("\nmap api...")
-    Hosts(Transforms.map("api.api.api", "192.169.0.202"))
-    printres
-    println("\nclear")
-    Hosts(Transforms.clear)
-    printres
+    def printres = println(Read(new java.io.File("test.out")))
+
+    def trytrans(name: String, op: Transforms.Op) {
+      println("\n%s" format name)
+      Hosts(op)
+      printres
+    }
+
+    trytrans("clear", Transforms.clear)
+
+    trytrans("append", Transforms.append(Section("appended", Nil)))
+
+    trytrans("remove", Transforms.remove("api"))
+
+    trytrans("map", Transforms.map("api.host.io", "192.169.0.202"))
+
+    // unmap
+
+    trytrans("ip", Transforms.ip("127.0.0.1", { println(_) }))
+
+    trytrans("host", Transforms.host("foo.com", { println(_) }))
   }
 }
