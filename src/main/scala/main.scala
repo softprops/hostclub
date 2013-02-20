715cbaf8f206938d45bd62df0d3815db3b1b7604
softprops <d.tangren@gmail.com>
2013-02-20 11:47:18 -0500

    changes to new hosts interface

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -6,22 +6,23 @@ object Main {
 
     def trytrans(name: String, op: Transforms.Op) {
       println("\n%s" format name)
-      Hosts(op)
+      Hosts(op)("test.out")
+      println("")
       printres
     }
 
-    trytrans("clear", Transforms.clear)
+  //  trytrans("clear", Transforms.clear)
 
-    trytrans("append", Transforms.append(Section("appended", Nil)))
+//    trytrans("append", Transforms.append(Section("appended", Nil)))
 
-    trytrans("remove", Transforms.remove("api"))
+//    trytrans("remove", Transforms.remove("api"))
 
     trytrans("map", Transforms.map("api.host.io", "192.169.0.202"))
 
     // unmap
 
-    trytrans("ip", Transforms.ip("127.0.0.1", { println(_) }))
+  //  trytrans("ip", Transforms.ip("127.0.0.1", { println(_) }))
 
-    trytrans("host", Transforms.host("foo.com", { println(_) }))
+    //trytrans("host", Transforms.host("foo.com", { println(_) }))
   }
 }
