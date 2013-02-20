3a4a8f89d6a1307316be466503f2f663403d2f87
softprops <d.tangren@gmail.com>
2013-02-20 15:34:36 -0500

    simple grep

--- src/main/scala/hosts.scala
+++ src/main/scala/hosts.scala
@@ -4,6 +4,33 @@ import java.io.{ File, FileWriter }
 import scala.io.Source
 
 object Hosts {
+   case class Grep(file: String) {
+
+    def host(in: String): Seq[String] =
+      resolve(file).map { f =>
+        Parse(Read(f)).fold(_ => Seq.empty[String], { cs =>
+          (Set.empty[String] /: cs) {
+            case (set, Section(_, mappings)) =>
+              val hs: Set[String] = (mappings.flatMap { case (_, hosts) => hosts.filter(_.startsWith(in)) }).toSet
+              set ++ hs
+            case (s, _) => s
+          }.toSeq
+        })
+      }.getOrElse(Seq.empty[String])
+
+    def ip(in: String): Seq[String] =
+      resolve(file).map { f =>
+        Parse(Read(f)).fold(_ => Seq.empty[String], { cs =>
+          (Set.empty[String] /: cs) {
+            case (set, Section(_, mappings)) =>
+              val ips = mappings.map { case (ip, _) => ip }
+              set ++ ips
+            case (s, _) => s
+          }.toSeq
+        })
+      }.getOrElse(Seq.empty[String])
+  }
+  def grep(file: String = "/etc/hosts") = Grep(file)
   def apply(op: Transforms.Op)(file: String = "/etc/hosts") =
     resolve(file).map { f =>
       Parse(Read(f)).fold(println, { chunks =>
